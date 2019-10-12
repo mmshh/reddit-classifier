443c5aa34f166fdfa97d22b71ce3d1a9855d05b1
jessica <jessgauvin@hotmail.com>
2019-10-12 16:29:57 -0400

    Second basic algo

--- classifier.py
+++ classifier.py
@@ -1,9 +1,19 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
 import numpy as np
 import csv
 
 
+# In[42]:
+
+
 def read_test_data():
-    return np.load("./resources/data_test.pkl", allow_pickle=True)
+    value = np.load("./resources/data_test.pkl", allow_pickle=True)
+    return value
 
 
 def classify_randomly(data):
@@ -20,6 +30,30 @@ def classify_randomly(data):
 
     return predictions
 
+def classify_2(data):
+    subreddits = ['AskReddit', 'GlobalOffensive', 'Music', 'Overwatch', 'anime',
+       'baseball', 'canada', 'conspiracy', 'europe', 'funny',
+       'gameofthrones', 'hockey', 'leagueoflegends', 'movies', 'nba',
+       'nfl', 'soccer', 'trees', 'worldnews', 'wow']
+    
+    np.random.seed(0)
+    random_ints = np.random.randint(0, 20, len(data))
+    print(np.bincount(random_ints))
+    predictions = []
+    
+    
+    for i in range(len(data)):
+        result = np.zeros(20)
+        subText = data[i].split()
+        for j in range(len(subText)):
+            if (subText[j] in subreddits):
+                result[subreddits.index(subText[j])] += 1
+        if(np.any(result)):
+            predictions.append({'Id': i, 'Category': subreddits[np.argmax(result)]})
+        else:
+            predictions.append({'Id': i, 'Category': subreddits[random_ints[i]]})
+            
+
 
 def convert_to_csv(data):
     csv_columns = ['Id', 'Category']
@@ -31,5 +65,6 @@ def convert_to_csv(data):
 
 if __name__ == "__main__":
     X_test = read_test_data()
-    predictions = classify_randomly(X_test)
+    predictions = classify_2(X_test)
     convert_to_csv(predictions)
+
