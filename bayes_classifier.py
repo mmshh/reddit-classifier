f9d0783bf18174d6754804b387a248cb7cfdeaec
jessica <jessgauvin@hotmail.com>
2019-10-14 13:09:51 -0400

    Naive bayes without stop words and punctuation

--- bayes_classifier.py
+++ bayes_classifier.py
@@ -1,10 +1,9 @@
 import numpy as np
 from collections import defaultdict
 import nltk
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
 import operator
-import csv
-
-
 
 class BayesClassifier:
     def __init__(self):
@@ -16,15 +15,18 @@ class BayesClassifier:
     def train(self, comments, classes):
         classes_name, classes_count = np.unique(classes, return_counts=True)
         
+        #Compute prior
         for i in range(len(classes_name)):
             self.prior[classes_name[i]] = classes_count[i]/len(classes)
         
+        #Compute frequencies and class counts
         for i in range(len(comments)):
-            bag = nltk.tokenize.word_tokenize(comments[i])
+            bag = self.tokenize_words(comments[i])
             for w in bag:
                 self.word_frequency_class[classes[i]][w] += 1
                 self.word_count_class[classes[i]] += 1
-
+        
+        #Compute word probability for a given class
         for c in self.word_frequency_class.keys():
             for word in self.word_frequency_class[c].keys():
                 self.word_probability_class[c][word] = self.word_frequency_class[c][word] / self.word_count_class[c]
@@ -33,18 +35,38 @@ class BayesClassifier:
         result = []
         for i in range(len(comments)):
             class_probability = defaultdict(float)
-            words = nltk.tokenize.word_tokenize(comments[i])
+            words = self.tokenize_words(comments[i])
             for class_name in self.prior.keys():
-                product = 1.0
-                for word in words:
-                    product *= self.word_probability_class[class_name][word]
-                class_probability[class_name] = self.prior[class_name]*product
+                probX_knowingC = self.compute_probX_knowingC(class_name, words)
+                class_probability[class_name] = probX_knowingC*self.prior[class_name]
             result.append({'Id': i, 'Category': max(class_probability.items(), key=operator.itemgetter(1))[0]})
         return result
+    
+    def tokenize_words(self, words):
+        #Tokenize without ponctuation
+        tokenizer = RegexpTokenizer(r'\w+')
+        word_tokens = tokenizer.tokenize(words.lower())
+        #Remove stop words
+        stop_words = set(stopwords.words('english'))
+        filtered_words = [w for w in word_tokens if not w in stop_words]
+        return filtered_words
+    
+    def compute_probX_knowingC(self, class_name, words):
+        #If no word exists in class return 0
+        if (sum(self.word_probability_class[class_name][word] for word in words) == 0):
+            return 0
+        
+        #Else return the product of P(word|c) for all word in words
+        product_prob_x = 1.0
+        for word in words:
+            if(self.word_probability_class[class_name][word] != 0):
+                product_prob_x *= self.word_probability_class[class_name][word]
+        return product_prob_x
+        
             
-    def convert_to_csv(self, data):
+    def convert_to_csv(data):
         csv_columns = ['Id', 'Category']
-        with open('./resources/output2.csv', 'w') as f:
+        with open('./resources/output.csv', 'w') as f:
             writer = csv.DictWriter(f, csv_columns)
             writer.writeheader()
             writer.writerows(data)
@@ -52,8 +74,8 @@ class BayesClassifier:
 
 if __name__ == "__main__":
     train_data = np.load("./resources/data_train.pkl", allow_pickle=True)
-    test_data = np.load("./resources/data_test.pkl", allow_pickle=True)
+    test_data = np.load("./resources/data_train.pkl", allow_pickle=True)
     bayes_classifier = BayesClassifier()
     bayes_classifier.train(train_data[0][:], train_data[1][:])
-    predictions = bayes_classifier.predict(test_data)
+    predictions = bayes_classifier.predict(train_data[0][:])
     bayes_classifier.convert_to_csv(predictions)
