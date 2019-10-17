c560f1837b7e247487a83cd9e496e865c469cbf4
MShahi, Maziar <maziar_mshahi@manulife.com>
2019-10-17 16:20:00 -0400

    readme and documentation of the code

--- bayes_classifier_with_smoothing.py
+++ bayes_classifier_with_smoothing.py
@@ -5,9 +5,11 @@ import random
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 import operator
-import csv
 
 from utils import convert_to_csv, read_train_data, read_test_data
+
+nltk.download('stopwords')
+
 
 class BayesClassifier:
     def __init__(self):
@@ -17,6 +19,12 @@ class BayesClassifier:
         self.word_count_class = defaultdict(int)
 
     def train(self, comments, classes):
+        """
+        Computes the prior and the word frequency in each class and total word count in each class
+        :param comments: raw comments
+        :param classes: classes
+        :return: void
+        """
         print('start training')
         classes_name, classes_count = np.unique(classes, return_counts=True)
         
@@ -34,7 +42,13 @@ class BayesClassifier:
                     self.voc.append(w)
             
     def predict(self, comments, alpha):
-        print('start predict')
+        """
+        Predicts the classes for the comments passed as the output using the alpha as the hyper parameter
+        :param comments: raw comments
+        :param alpha: the smoothing hyper param
+        :return: class predictions
+        """
+        print('start predictions')
         result = []
         for i in range(len(comments)):
             class_probability = defaultdict(float)
@@ -46,6 +60,11 @@ class BayesClassifier:
         return result
     
     def tokenize_words(self, words):
+        """
+        Tokenizes, removes the stop words and digits and outputs the stems of the words to produce the bag of words
+        :param words: the comments
+        :return: the bag of words
+        """
         #Tokenize without ponctuation
         tokenizer = RegexpTokenizer(r'\w+')
         word_tokens = tokenizer.tokenize(words.lower())
@@ -56,26 +75,36 @@ class BayesClassifier:
                           if w not in stop_words and not w.isdigit()]
         return filtered_words
     
-    def compute_probX_knowingC(self, class_name, words, alpha):      
-        #Return the product of P(word|c) for all word in words
+    def compute_probX_knowingC(self, class_name, words, alpha):
+        """
+        Returns the log probability of P(word|class) for all words in the bag of words
+        :param class_name: class name
+        :param words: bag of words
+        :param alpha: hyper param
+        :return: P(word|c)
+        """
         product_prob_x = 0.
         for word in words:
             product_prob_x += np.log(self.compute_probx_knowingC(class_name, word, alpha))
         return product_prob_x
-    
+
     def compute_probx_knowingC(self, class_name, word, alpha):
         #Compute word probability for a given class
         return (self.word_frequency_class[class_name][word]+alpha) / (self.word_count_class[class_name]+alpha*len(self.voc))
 
-    
     def split_data(self, data_c, data_r, train_percent):
+        """
+        Helper function to validate the data for validation
+        """
         data_comments = np.array(data_c)
         data_class = np.array(data_r)
         random_indices = np.array(random.sample(range(len(data_c)), int(len(data_c)*train_percent)))
         return data_comments[random_indices], data_class[random_indices], data_comments[~random_indices], data_class[~random_indices]
-        
-        
+
     def score(self, predictions, result):
+        """
+        Helper function to check accuracy for validation
+        """
         count = 0
         for i in range(len(predictions)):
             if(predictions[i]['Category'] == result[i]):
@@ -83,12 +112,15 @@ class BayesClassifier:
         return count/len(predictions)
 
     def define_alpha(self, validation_comments, validation_result):
+        """
+        Helper function to find a good value for hyper param alpha
+        """
         alpha = [0.00001, 0.00005, 0.0001, 0.0005, 0.001, 0.005, 0.01]
         result = np.zeros(len(alpha))
         for i in range(len(alpha)):
             print('Alpha ',i+1,'/',len(alpha),' : ',alpha[i])
-            predict = bayes_classifier.predict(v_c, alpha[i])
-            result[i] = bayes_classifier.score(predict, v_r)
+            predict = bayes_classifier.predict(validation_comments, alpha[i])
+            result[i] = bayes_classifier.score(predict, validation_result)
             print(result[i])
         print(result)
         print(alpha[np.argmax(result)])
@@ -105,4 +137,3 @@ if __name__ == "__main__":
     bayes_classifier.train(comment, result)
     predictions = bayes_classifier.predict(test_data, alpha_star)
     convert_to_csv(predictions)
-    
